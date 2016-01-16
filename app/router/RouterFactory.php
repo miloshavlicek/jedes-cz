622efcfbdb34d64be65abadc07db2b4851e1aa89
Miloš Havlíček <miloshavlicek@gmail.com>
2016-01-16 17:42:14 +0100

    + web.config, Route::SECURED

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -12,9 +12,11 @@ class RouterFactory {
      * @return Nette\Application\IRouter
      */
     public static function createRouter() {
+        $flag = in_array($_SERVER['REMOTE_ADDR'], ['::1', '127.0.0.1']) ? NULL : Route::SECURED;
+
         $router = new RouteList;
-        $router[] = new Route('o-aplikaci', 'About:default');
-        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default');
+        $router[] = new Route('o-aplikaci', 'About:default', $flag);
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default', $flag);
         return $router;
     }
 
